510df7995554342800834b0a29c082e55a493aec
SLimTeARiK <laitkorsunow@gmail.com>
2025-06-19 18:45:10 +0300

    v0.0.1

--- build/clang/_deps/highway-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/Desktop/Programming/Projects/neural_network_SDL/build/clang/_deps/highway-src
-# Build directory: C:/Users/user/Desktop/Programming/Projects/neural_network_SDL/build/clang/_deps/highway-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
